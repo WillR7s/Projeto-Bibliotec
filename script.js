6ed7d233e495cb61784be46a650eafe09d18e1a9
WILLIAN RIBEIRO DOS SANTOS <willrs780@gmail.com>
2026-09-11 10:59:58 -0300

    conceitos sobre js

---
+++ script.js
@@ -0,0 +1,40 @@
+let nome="Willian";
+let idade= 25;
+
+//concatenação
+console.log("Nome:" + nome +  ", idade:" + idade);
+
+//Interpolação
+console.log(`Nome: ${nome}, idade: ${idade}`);
+
+
+//operadores relacionair
+
+//valida igualdade de valor
+//"10" == 10 -> true
+
+
+//valida igualdade de valor e tipo de dado
+//"10" --- 10 -> false
+
+
+
+//let opcao =2 
+
+//switch(opcao){
+   // case 1: ifhoifhhfi
+    //break;
+//}
+
+
+//Estrutura de repetiçao
+
+let frutas = ["Maçã", "Banana", "Acerola", "Uva"]
+
+console.log(frutas);
+
+frutas.forEach(function(fruta){
+    console.log("Nome: " + fruta);
+}) 
+
+//frutas.forEach(fruta=> console.log("Nome: " + fruta))
